02a0e7a90752dea93d2e00bcd65f7dbadb1dd18f
김성민 <neraziim@gmail.com>
2026-03-27 13:39:14 +0900

    feat/refactor: submission.repository 최적화
    - feat: 제출물 중복 제출 방지 메소드(findIfUserSubmit) 추가
    - feat: 관리자 여부에 따라 소프트삭제된 데이터 노출 분기 처리하는 로직 추가
    - refactor: #whereCase 프라이빗 메소드를 통해 필터링 로직 공통화
    - refactor: 베스트 제출물 목록 조회 로직에 Prisma groupBy 메소드 적용하여 효율성 개선

--- src/repository/submission.repository.js
+++ src/repository/submission.repository.js
@@ -1,122 +1,122 @@
-const submissionSelect = {
-  id: true,
-  userId: true,
-  title: true,
-  content: true,
-  createdAt: true,
-  updatedAt: true,
-};
-
-const submissionDetailSelect = {
-  id: true,
-  title: true,
-  content: true,
-  createdAt: true,
-  updatedAt: true,
-  isDeleted: true,
-  user: {
-    select: {
-      id: true,
-      nickname: true,
-      grade: true,
-    },
-  },
-};
-
 export class SubmissionRepository {
   #prisma;
-
+  #whereCase(filter, { isAdmin = false } = {}) {
+    return {
+      ...filter,
+      ...(isAdmin ? {} : { isDeleted: false }),
+    };
+  }
   constructor({ prisma }) {
     this.#prisma = prisma;
   }
 
-  //챌린지의 작업물 전체 조회
-  findAll(id, { page = 1, limit = 10 }) {
-    const skip = (page - 1) * limit;
-    const where = { challengeId: id };
+  get #submissionSelect() {
+    return {
+      id: true,
+      challengeId: true,
+      userId: true,
+      title: true,
+      content: true,
+      isDeleted: true,
+      createdAt: true,
+      updatedAt: true,
+      user: {
+        select: {
+          id: true,
+          nickname: true,
+          grade: true,
+        },
+      },
+      _count: {
+        select: { likes: true, feedbacks: true },
+      },
+    };
+  }
 
-    return Promise.all([
+  //챌린지별 작업물 목록 조회
+  async findAll(id, { page = 1, limit = 10, isAdmin = false } = {}) {
+    const skip = (page - 1) * limit;
+    const where = this.#whereCase({ challengeId: id }, { isAdmin });
+
+    return await Promise.all([
       this.#prisma.submission.findMany({
         where,
         skip,
         take: limit,
-        select: submissionSelect,
+        orderBy: {
+          likes: {
+            _count: 'desc',
+          },
+        },
+        select: this.#submissionSelect,
       }),
       this.#prisma.submission.count({ where }),
     ]);
   }
 
-  //베스트 작업물 후보 조회
-  async findTopLiked(id) {
-    const all = await this.#prisma.submission.findMany({
+  //베스트 작업물 목록 조회
+  async findBestList(id) {
+    const listLikeDesc = await this.#prisma.submissionLike.groupBy({
+      by: ['submissionId'],
+      where: { challengeId: id, isDeleted: false },
+      _count: { _all: true },
+      orderBy: { _count: { _all: 'desc' } },
+      take: 1,
+    });
+
+    if (listLikeDesc.length === 0) return [];
+
+    const maxLikes = listLikeDesc[0]._count._all;
+
+    return await this.#prisma.submission.findMany({
       where: {
         challengeId: id,
         isDeleted: false,
+        likes: { _count: maxLikes },
       },
-      orderBy: {
-        likes: {
-          _count: 'desc',
-        },
-      },
-      select: {
-        ...submissionDetailSelect,
-        _count: { select: { likes: true } },
-      },
+      select: this.#submissionSelect,
     });
-
-    if (all.length === 0) return [];
-
-    const top1 = all[0]._count.likes;
-
-    return all.filter((x) => x._count.likes === top1);
   }
 
   //작업물 상세 조회
   findById(id) {
-    return this.#prisma.submission.findUnique({
-      where: {
-        id,
+    return this.#prisma.submission.findFirst({
+      where: this.#whereCase({ id }),
+      select: {
+        ...this.#submissionSelect,
       },
-      select: submissionDetailSelect,
+    });
+  }
+
+  //기 제출 여부 확인
+  async findIfUserSubmit(challengeId, userId) {
+    return await this.#prisma.submission.findFirst({
+      where: { challengeId, userId, isDeleted: false },
     });
   }
 
   //작업물 생성
-  create(id, data) {
+  create(data) {
     return this.#prisma.submission.create({
-      data: {
-        ...data,
-        challengeId: id,
-      },
-      select: {
-        ...submissionSelect,
-        challengeId: true,
-        isDeleted: true,
-      },
+      data,
+      select: this.#submissionSelect,
     });
   }
 
   //작업물 수정
   update(id, data) {
     return this.#prisma.submission.update({
-      where: {
-        id,
-      },
+      where: this.#whereCase({ id }),
       data,
-      select: {
-        ...submissionSelect,
-        challengeId: true,
-      },
+      select: this.#submissionSelect,
     });
   }
 
   //작업물 삭제 (소프트 삭제)
-  updateToDeleted(id, { isDeleted }) {
+  updateToDeleted(id) {
     return this.#prisma.submission.update({
-      where: {
-        id,
-      },
-      data: { isDeleted },
+      where: this.#whereCase({ id }),
+      data: { isDeleted: true },
       select: {
         id: true,
         isDeleted: true,
